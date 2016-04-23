095cdd621aca2001aabf55a2d1acac2cdbf7d87f
Marcin Paszkiewicz <mkpaszkiewicz@gmail.com>
2016-04-24 01:47:30 +0200

    Tokens definition

--- html_template_parser/lexer.py
+++ html_template_parser/lexer.py
@@ -1,3 +1,55 @@
+from html_template_parser.token import Token
+
+tokens = {
+    'macro': Token.Token.MACRO,
+    'endmacro': Token.Token.ENDMACRO,
+    'set': Token.SET,
+    'if': Token.IF,
+    'elif': Token.ELIF,
+    'else': Token.ELSE,
+    'endif': Token.ENDIF,
+    'for': Token.FOR,
+    'endfor': Token.ENDFOR,
+    'in': Token.IN,
+
+    'and': Token.AND,
+    'or': Token.OR,
+    'not': Token.NOT,
+
+    '+': Token.PLUS,
+    '-': Token.MINUS,
+    '/': Token.SLASH,
+    '%': Token.MOD,
+    '*': Token.STAR,
+
+    '=': Token.ASSIGN,
+    '<': Token.LT,
+    '>': Token.GT,
+    '<=': Token.LE,
+    '>=': Token.GE,
+    '==': Token.EQ,
+    '!=': Token.NEQ,
+
+    ',': Token.COMMA,
+    '.': Token.DOT,
+    '(': Token.LEFT_BRACKET,
+    ')': Token.RIGHT_BRACKET,
+    '[': Token.LEFT_SQUARE_BRACKET,
+    ']': Token.RIGHT_SQUARE_BRACKET,
+
+    '{%': Token.TEMPLATE_OPEN,
+    '}%': Token.TEMPLATE_CLOSE,
+    '{{': Token.STATEMENT_OPEN,
+    '}}': Token.STATEMENT_CLOSE,
+    '{#': Token.COMMENT_OPEN,
+    '#}': Token.COMMENT_CLOSE,
+
+    ' ': Token.WHITESPACE,
+    '\n': Token.Token.WHITESPACE,
+    '\t': Token.Token.WHITESPACE
+}
+
+
 class SourceController:
     """Class reads from input stream and place characters in buffer.
     Stores information about line and position."""
